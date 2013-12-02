07950adc9aa972dfce8d49e37768baa8503e20e6
Felipe Torres <torresfelipex1@gmail.com>
2013-12-02 17:24:51 +0100

    modified:   app/hello.c
    Añadido el código para la segunda parte de la práctica 5.
    Controlar la ilumnación de los leds de la placa en función
    del botón pulsado, s2 para verde y s3 para rojo.

--- app/hello.c
+++ app/hello.c
@@ -1,7 +1,7 @@
 /*****************************************************************************/
 /*                                                                           */
 /* Sistemas Empotrados                                                       */
-/* El "hola mundo" en la Redwire EconoTAG en C                               */
+/* Ejercicio 2 de la práctica 5                                              */
 /*                                                                           */
 /*****************************************************************************/
 
@@ -11,17 +11,36 @@
  * Constantes relativas a la plataforma
  */
 
+/* Dirección del registro de control de dirección del GPIO 0-GPIO 31 */
+volatile uint32_t * const reg_gpio_pad_dir0    = (uint32_t *) 0x80000000;
+
 /* Dirección del registro de control de dirección del GPIO32-GPIO63 */
 volatile uint32_t * const reg_gpio_pad_dir1    = (uint32_t *) 0x80000004;
 
+/* Dirección del registro para consultar el estado de los pulsadores
+   GPIO DATA 00-31 */
+volatile uint32_t * const reg_gpio_data0       = (uint32_t *) 0x80000008;
+
+/* Dirección del registro de activación de los botones GPIO 00-31 */
+volatile uint32_t * const reg_gpio_data_set0   = (uint32_t *) 0x80000048;
+
 /* Dirección del registro de activación de bits del GPIO32-GPIO63 */
+// Para los LEDs
 volatile uint32_t * const reg_gpio_data_set1   = (uint32_t *) 0x8000004c;
 
 /* Dirección del registro de limpieza de bits del GPIO32-GPIO63 */
 volatile uint32_t * const reg_gpio_data_reset1 = (uint32_t *) 0x80000054;
 
-/* El led rojo está en el GPIO 44 (el bit 12 de los registros GPIO_X_1) */
-uint32_t const led_red_mask = (1 << (44-32));
+// El led rojo está en el GPIO 44 (el bit 12 de los registros GPIO_X_1) */
+uint32_t const led_red_mask     = (1 << (44-32));
+// El led verde está en el GPIO 45
+uint32_t const led_green_mask   = (1 << (45-32));
+
+// Máscaras para los botones
+uint32_t const btn_22_ou        = 0x00400000;
+uint32_t const btn_23_ou        = 0x00800000;
+uint32_t const btn_26_in        = 0x04000000;
+uint32_t const btn_27_in        = 0x08000000;
 
 /*
  * Constantes relativas a la aplicacion
@@ -35,8 +54,13 @@ uint32_t const delay = 0x10000;
  */
 void gpio_init(void)
 {
-	/* Configuramos el GPIO44 para que sea de salida */
-	*reg_gpio_pad_dir1 = led_red_mask;
+	/* Configuramos el GPIO44,45 para que sea de salida */
+	*reg_gpio_pad_dir1 = *reg_gpio_pad_dir1 | led_red_mask | led_green_mask;
+	
+	// Configuramos el GPIO22,23
+	*reg_gpio_pad_dir0 = *reg_gpio_pad_dir0 | btn_22_ou | btn_23_ou;
+	
+	*reg_gpio_data_set0 = *reg_gpio_data_set0 | btn_22_ou | btn_23_ou;
 }
 
 /*****************************************************************************/
@@ -48,7 +72,7 @@ void gpio_init(void)
 void leds_on (uint32_t mask)
 {
 	/* Encendemos los leds indicados en la máscara */
-	*reg_gpio_data_set1 = mask;
+	*reg_gpio_data_set1 = *reg_gpio_data_set1 | mask;
 }
 
 /*****************************************************************************/
@@ -70,7 +94,7 @@ void leds_off (uint32_t mask)
  */
 void pause(void)
 {
-        uint32_t i;
+    uint32_t i;
 	for (i=0 ; i<delay ; i++);
 }
 
@@ -88,10 +112,21 @@ int main ()
 {
 	gpio_init();
 
-        the_led = led_red_mask;
+    typedef enum {green, red} led;
+    led bombillita = red;
+    uint32_t pulsados;
 
 	while (1)
 	{
+	    pulsados = *reg_gpio_data0;
+	    
+	    if(pulsados & btn_26_in)
+	        bombillita = green;
+        else if(pulsados & btn_27_in)
+                bombillita = red;
+                
+        the_led = bombillita == green? led_green_mask : led_red_mask;
+        
 		leds_on(the_led);
                 pause();
 
